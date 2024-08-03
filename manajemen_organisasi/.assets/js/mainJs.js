7ce7ae048a149a2b3d6047447ed5990dd439ea44
dhwalief <dhiwa.bakar@gmail.com>
2024-08-03 13:49:14 +0800

    edit

--- manajemen_organisasi/.assets/js/mainJs.js
+++ manajemen_organisasi/.assets/js/mainJs.js
@@ -10,3 +10,27 @@ menubtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     sideBar.style.display = 'none';
 })
+
+
+// navbar
+document.addEventListener("DOMContentLoaded", (event) => {
+    const profileLink = document.getElementById("profile-link");
+    const dropdown = document.getElementById("dropdown");
+  
+    profileLink.addEventListener("click", (event) => {
+      event.preventDefault();
+      const isDropdownVisible = dropdown.style.display === "block";
+      dropdown.style.display = isDropdownVisible ? "none" : "block";
+    });
+  
+    // Close the dropdown if user clicks outside of it
+    document.addEventListener("click", (event) => {
+      if (
+        !profileLink.contains(event.target) &&
+        !dropdown.contains(event.target)
+      ) {
+        dropdown.style.display = "none";
+      }
+    });
+  });
+  
